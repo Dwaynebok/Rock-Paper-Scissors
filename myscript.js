1940cfddd28dedb319a78b56a84f71d5e2f2349b
Dwaynebok <dwaynebok19@gmail.com>
2020-09-10 17:26:15 +0200

    changed the method of code because of mistakes made

--- myscript.js
+++ myscript.js
@@ -13,12 +13,76 @@
         alert("Lets see the god in you!");
     }
 
+    let playerChoice = "";
+    let computerChoice = "";
+    let randomNumber = "";
 
+    document.getElementById("rockOne").addEventListener("click", function (){
+        playerChoice = "rock";
+        console.log(playerChoice);
+    })
+
+    document.getElementById("scissorsOne").addEventListener("click", function (){
+        playerChoice = "scissors";
+        console.log(playerChoice);
+    })
+
+    document.getElementById("paperOne").addEventListener("click", function (){
+        playerChoice = "paper";
+        console.log(playerChoice);
+
+    })
+
+    document.getElementById("computerPick").addEventListener("click", function (){
+        randomNumber= Math.floor((Math.random()*3)+1);
+        console.log(randomNumber);
+        if (randomNumber == 1){
+            computerChoice = 'rock';
+            console.log(computerChoice);
+        }
+        else if(randomNumber == 2){
+            computerChoice = 'paper'
+            console.log(computerChoice);
+        }
+        
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const notification = document.querySelector('.Notification');
 const points = document.querySelector('.points');
 const buttons = document.querySelectorAll('button');
-const winnerPoints = [0,0];
-const winChecker = 
+
+ */
+/*
 
 //added event listner to the buttons
 for (let i = 0; i > buttons.length; i++){
@@ -30,15 +94,39 @@ function startGame(playerOne) {
     let computerSelect = Math.random();
 
     if (computerSelect < .22) {
-        computerSelect = 'Paper';
+        playerSelect = 'Paper';
     } else if (computerSelect <= .35) {
-        computerSelect = 'scissors';
+        playerSelect = 'scissors';
     } else {
-        computerSelect = 'scissors';
+        playerSelect= 'scissors';
     }
 }
 
-let results = winChecker('computerSelect' , 'playerSelect');
+function winChecker(player, computer){
+    if (player === computer){
+        results += 'draw';
+    }
+    if (player === 'Rock'){
+        if (computer === 'paper'){
+            return 'Computer';
+        }
+    }
+
+    if (player === 'paper'){
+        if (computer === 'Scissors'){
+            return 'Computer'
+        }
+    }
+
+    if ( player === 'Scissors'){
+        if ( computer === 'Rock'){
+            return 'Player'
+        }
+    }
+
+}
+
+let results = winChecker(computerSelect , playerSelect);
     if (results === 'computer'){
         results += 'Are you sure you can Win!';
         winnerPoints[0]++;
@@ -50,6 +138,14 @@ let results = winChecker('computerSelect' , 'playerSelect');
     if (results === 'draw'){
         results += 'lets Strike again!'
     }
+
+   points.innerHTML = 'Player: [ ' + winnerPoints[0]+ ' ] Computer: [ ' + winnerPoints[1] + ' ]';
+ console.log(playerSelect);
+
+ */
+
+
+
 
 })();
 
