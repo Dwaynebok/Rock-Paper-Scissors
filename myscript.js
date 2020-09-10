09f85726875f668cdb26b3fe952f701e7d049d88
Dwaynebok <dwaynebok19@gmail.com>
2020-09-10 13:52:53 +0200

    started with a bit of html css and javascript

---
+++ myscript.js
@@ -0,0 +1,27 @@
+ alert("lets get Ready to Rumble");
+
+(function chooseONe(){
+
+    var decision = window.prompt("WHat level are you?")
+    if (decision == "beginner") {
+        alert("Lets Begin!");
+    }
+    if (decision == "intermediate") {
+        alert("Lets see what you can do!");
+    }
+    if (decision == "expert") {
+        alert("Lets see the god in you!");
+    }
+})();
+
+const notification = document.querySelector('.Notification');
+const points = document.querySelector('.points');
+const buttons = document.querySelectorAll('button');
+
+
+
+
+
+
+
+
