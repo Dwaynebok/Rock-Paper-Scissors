6b84a151199aa78d36bc7074914cf9d69bafcebd
Dwaynebok <dwaynebok19@gmail.com>
2020-09-11 22:02:31 +0200

    adding more style and hanged some of my if statements

--- myscript.js
+++ myscript.js
@@ -22,31 +22,16 @@ function chooseONe() {
 let rock = document.getElementById("rock").onclick = playGame;
 let paper = document.getElementById("paper").onclick = playGame;
 let scissors = document.getElementById("scissors").onclick = playGame;
-
+let scoreDraw = 0;
 let scoreComputer = 0;
 let scorePLayer = 0;
+let rounds = 1;
 
 
 
 function playGame() {
 
 
-
-
-
-
-
-
-
-    // let playerChoice = "";
-    // let computerChoice = "";
-    //let randomNumber = "";
-    //
-
-
-    // document.getElementById("computerPick").addEventListener("click", function () {
-    //   randomNumber = Math.floor((Math.random() * 3) + 1);
-    // document.getElementById("choice-computer").innerText = "computer picks " + computerChoice
 
     let uof = this.id
     document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
@@ -67,12 +52,14 @@ function playGame() {
 
     if (computerChoice == uof) {
         console.log("Its a tie");
-        document.getElementById("draw").innerHTML = 'its a tie!!!!'
+        scoreDraw++;
+
     }
 
     if (uof == 'scissors' && computerChoice == 'paper') {
         console.log('scissors');
         scorePLayer++;
+        rounds++;
         document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
         document.getElementById("choice-computer").innerHTML = `computer has  picked ${computerChoice}`
         countingWins();
@@ -80,6 +67,7 @@ function playGame() {
     } else if (uof == "scissors" && computerChoice == "rock") {
         console.log('rock');
         scoreComputer++;
+        rounds++;
         document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
         document.getElementById("choice-computer").innerHTML = `computer has  picked ${computerChoice}`
         countingWins();
@@ -88,6 +76,7 @@ function playGame() {
     if (uof == "paper" && computerChoice == "rock") {
         console.log('paper');
         scorePLayer++;
+        rounds++;
         document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
         document.getElementById("choice-computer").innerHTML = `computer has  picked ${computerChoice}`
         countingWins();
@@ -96,73 +85,50 @@ function playGame() {
     if (uof == "paper" && computerChoice == "scissors") {
         console.log('scissors');
         scoreComputer++;
-        document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
-        document.getElementById("choice-computer").innerHTML = `computer has  picked ${computerChoice}`
-        countingWins();
-    } else if (uof == 'rock' && computerChoice == 'paper') {
-        console.log('paper');
-        scoreComputer++;
+        rounds++;
         document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
         document.getElementById("choice-computer").innerHTML = `computer has  picked ${computerChoice}`
         countingWins();
     }
-        // if(playerChoice=="paper" && computerChoice=="scissors"){
-        //     console.log('scissors');
-        //    scoreComputer++;
-        //countingWins();
-    // }
+    else if (uof == 'rock' && computerChoice == 'paper') {
+        console.log('paper');
+        scoreComputer++;
+        rounds++;
+        document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
+        document.getElementById("choice-computer").innerHTML = `computer has  picked ${computerChoice}`
+        countingWins();
+    }
+
     else if (uof == "rock" && computerChoice == "scissors") {
         console.log('rock');
         scorePLayer++;
+        rounds++;
         document.getElementById("choice-player").innerHTML = `You have picked ${uof}`
         document.getElementById("choice-computer").innerHTML = `computer has  picked ${computerChoice}`
         countingWins();
     }
 
     function countingWins() {
-        document.getElementById("target-comp").innerText = "Computer " + scoreComputer + " wins";
-        document.getElementById("target-you").innerText = " player " + scorePLayer + "wins";
+        document.getElementById("target-comp").innerText = "Computer " + scoreComputer + " wins ";
+        document.getElementById("target-you").innerText = " player " + scorePLayer + " wins ";
+        document.getElementById("draw").innerText = " draw " + scoreDraw ;
+        document.getElementById("rounds").innerText = " rounds " + rounds;
+
 
     }
 
-
-    //  })
-
-
 }
-
-
-
-      /*
-        document.getElementById("rockOne").addEventListener("click", function () {
-            playerChoice = "rock";
-            console.log('playerPicks' + playerChoice);
-            document.getElementById("choice-player").innerText = "Player picks " + playerChoice;
-        })
-
-        document.getElementById("scissorsOne").addEventListener("click", function () {
-            playerChoice = "scissors";
-            console.log('playerPicks' + playerChoice);
-            document.getElementById("choice-player").innerText = "Player picks " + playerChoice;
-        })
-
-        document.getElementById("paperOne").addEventListener("click", function () {
-            playerChoice = "paper";
-            console.log('playerPicks' + playerChoice);
-            document.getElementById("choice-player").innerText = "Player picks " + playerChoice;
-
-        })
-
-       */
+//reset Button
 
 const resetButton = document.querySelector(".resetOne");
 const refreshPage = () =>{
     location.reload();
 
 }
-      resetButton.addEventListener("click", refreshPage);
+resetButton.addEventListener("click", refreshPage);
 
 
+//
 
 
 
@@ -194,147 +160,15 @@ const refreshPage = () =>{
 
 
 
-/*
-const notification = document.querySelector('.Notification');
-const points = document.querySelector('.points');
-const buttons = document.querySelectorAll('button');
 
- */
-/*
 
-//added event listner to the buttons
-for (let i = 0; i > buttons.length; i++){
-    buttons[i].addEventListener("click", startGame);
-}
 
-function startGame(playerOne) {
-    let playerSelect = playerOne.target.innerText;
-    let computerSelect = Math.random();
 
-    if (computerSelect < .22) {
-        playerSelect = 'Paper';
-    } else if (computerSelect <= .35) {
-        playerSelect = 'scissors';
-    } else {
-        playerSelect= 'scissors';
-    }
-}
 
-function winChecker(player, computer){
-    if (player === computer){
-        results += 'draw';
-    }
-    if (player === 'Rock'){
-        if (computer === 'paper'){
-            return 'Computer';
-        }
-    }
 
-    if (player === 'paper'){
-        if (computer === 'Scissors'){
-            return 'Computer'
-        }
-    }
 
-    if ( player === 'Scissors'){
-        if ( computer === 'Rock'){
-            return 'Player'
-        }
-    }
 
-}
 
-let results = winChecker(computerSelect , playerSelect);
-    if (results === 'computer'){
-        results += 'Are you sure you can Win!';
-        winnerPoints[0]++;
-    }
-    if (results === 'player'){
-        results += 'Dont underestimate Me!';
-        winnerPoints[1]++;
-    }
-    if (results === 'draw'){
-        results += 'lets Strike again!'
-    }
-
-   points.innerHTML = 'Player: [ ' + winnerPoints[0]+ ' ] Computer: [ ' + winnerPoints[1] + ' ]';
- console.log(playerSelect);
-
- */
-
-
-
-
-
-
-
-/*
-        let selection = ['rockOne','paperOne','scissorsOne'];
-let scorePlayer  = 0;
-let scoreComputer = 0;
-
-
-let rock = document.getElementById("rockOne");
-let paper = document.getElementById("paperOne");
-let scissors = document.getElementById("scissorsOne");
-
-
-rock.addEventListener("click", function (){
-    let randomIndex = Math.floor(Math.random()*selection.length -1);
-    if (selection[randomIndex] == rock.id){
-        scorePlayer++;
-        document.querySelector("#target-comp").innerHTML = 'you win';
-    }else {
-        scoreComputer++;
-        document.querySelector("#target-comp").innerHTML = 'computer wins';
-    }
-})
-
-paper.addEventListener("click", function (){
-    let randomIndex = Math.floor(Math.random()*selection.length -1);
-    if (selection[randomIndex] == paper.id){
-        scorePlayer++;
-        document.querySelector("#target-comp").innerHTML = 'you win';
-    }else {
-        scoreComputer++;
-        document.querySelector("#target-comp").innerHTML = 'computer wins';
-    }
-})
-
-scissors.addEventListener("click", function (){
-    let randomIndex = Math.floor(Math.random()*selection.length -1);
-    if (selection[randomIndex] == scissors.id){
-        scorePlayer++;
-        document.querySelector("#target-comp").innerHTML = 'you win';
-    }else {
-        scoreComputer++;
-        document.querySelector("#target-comp").innerHTML = 'computer wins';
-    }
-})
-
-document.getElementById("resetOne").addEventListener("click", function (){
-    document.getElementById("player-score").innerText = `player score ${scorePlayer}` ;
-    document.getElementById("computer-score").innerText = `computer score ${scoreComputer}`;
-    if(scorePlayer > scoreComputer){
-        document.querySelector("#target-comp").innerHTML = 'you win';
-    }
-    else{
-        document.querySelector("#target-comp").innerHTML = 'computer wins';
-    }
-})
-
-
-
-
-
-// create an Arr of the selections
- // select at random element from the Arr
- // buttons to should have a unique name
- //if the random selector is the same as the unique button we display win
- // if not then its a tie
- // count the scores
-
- */
 
 
 
